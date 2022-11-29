3945af4eb8b15278d2e38bf5b92a89dd60616f5f
ShimizuSKT <2507914528@qq.com>
2022-11-29 23:48:12 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+
+#define rep(k, l, j) for (int k = l; k <= j; k++)
+
+int main()
+{
+    SetConsoleOutputCP(65001);
+    while (1)
+    {
+        printf("欢迎使用中国矿业大学(北京)体育场馆预约系统\n");
+        printf("Welcome to China University of Mining and Technology (Beijing) Stadium Reservation System");
+        Sleep(3000);
+        system("cls");
+        //欢迎界面，并清屏
+        //接下来是身份确认部分，设计一个函数确认身份
+        system("cls");
+        //接下来是功能选择界面
+        system("cls");
+        //接下来是各功能模块函数
+        system("cls");
+        //设计选择模块时若输入1145141919810就进入debug模式
+        system("cls");
+        printf("感谢您的使用,系统正返回首页\n");
+        printf("Thank you for using the system, now back to homepage.\n");
+        Sleep(3000);
+        system("cls");
+    }
+    return 0;
+}
