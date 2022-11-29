d72e5cbf8c7a98cc129ea6334d828ed7be330858
ShimizuSKT <2507914528@qq.com>
2022-11-30 00:08:14 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 #define rep(k, l, j) for (int k = l; k <= j; k++)
 
+int logincount;//logincount记录登陆尝试次数
+
 int main()
 {
     SetConsoleOutputCP(65001);
@@ -15,6 +17,31 @@ int main()
         Sleep(3000);
         system("cls");
         //欢迎界面，并清屏
+        while (1)
+        {
+            logincount++;
+            printf("请输入您的帐号\n");
+            printf("Please enter your account to login\n");
+            printf("账号(Account):");
+
+            printf("\n请输入您的密码\n");
+            printf("Please enter your password\n");
+            printf("密码(Password):");
+            printf("\n确认中，请稍后\n");
+            printf("Checking your profile, please wait.\n");
+            rep(i, 1, 6){
+                Sleep(800);
+                printf(". ");
+            }printf("\n");
+            if(logincount == 5){
+                printf("您的尝试错误次数以超过五次,系统即将关闭\n");
+                printf("You have failed for five times, system closing.\n");
+                Sleep(3000);
+                system("cls");
+                return 0;
+            }
+            printf("\n");
+        }
         //接下来是身份确认部分，设计一个函数确认身份
         system("cls");
         //接下来是功能选择界面
